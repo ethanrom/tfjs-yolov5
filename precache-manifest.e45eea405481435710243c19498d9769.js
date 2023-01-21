ab77057cb9845c643b1866ced7df9063af77cb45
ethanrom <sandaruwan777j@gmail.com>
2023-01-22 00:54:11 +0530

    Updates

---
+++ precache-manifest.e45eea405481435710243c19498d9769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb62db0f37be7621095",
+    "url": "/tfjs-yolov5/static/css/main.33032fe3.chunk.css"
+  },
+  {
+    "revision": "dfb62db0f37be7621095",
+    "url": "/tfjs-yolov5/static/js/main.dfb62db0.chunk.js"
+  },
+  {
+    "revision": "d5556193f140be237e7b",
+    "url": "/tfjs-yolov5/static/js/1.d5556193.chunk.js"
+  },
+  {
+    "revision": "d263452142855012bcb1",
+    "url": "/tfjs-yolov5/static/js/runtime~main.d2634521.js"
+  },
+  {
+    "revision": "e7229b2eedce176ff48f604d1dfac9d9",
+    "url": "/tfjs-yolov5/index.html"
+  }
+];
